3fff6b8303db682b0870f50f62a75e3204aeb990
NicoleQiu99 <nicolepeng99@gmail.com>
2020-08-17 15:32:53 -0700

    basic

---
+++ slack-chat-frontend/src/components/Post.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+
+class Post extends Component {
+  currentPostInfo = () => {
+    let currentPost = this.props.posts.filter(
+      (item) => item.id === this.props.post.id
+    );
+    return (
+      <div>
+        <a class="PostInfo">
+          <img
+            src={currentPost.length > 0 && currentPost[0].user.img_url}
+            width="30"
+            height="30"
+            class="d-inline-block align-top"
+            alt=""
+            loading="lazy"
+          />
+          {currentPost.length > 0 && currentPost[0].user.username}
+          {currentPost.length > 0 && currentPost[0].user.timestamp}
+        </a>
+        <p>{currentPost.length > 0 && currentPost[0].content}</p>
+      </div>
+    );
+  };
+
+  render() {
+    console.log(this.props);
+    return <div className="Post">{this.currentPostInfo()}</div>;
+  }
+}
+
+export default Post;
---
+++ slack-chat-frontend/src/components/PostForm.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+function PostForm(props) {
+  let currentChannel = props.channels.filter((channel) => {
+    return channel.id === parseInt(props.match.params.channelId);
+  });
+
+  let currentChannelName = currentChannel.length > 0 && currentChannel[0].name;
+  // console.log(currentChannelName);
+
+  return (
+    <form onSubmit={(e) => props.addPost(e)}>
+      <input
+        name="post"
+        type="text"
+        placeholder={"message#" + currentChannelName}
+      />
+      <input type="submit" />
+    </form>
+  );
+}
+
+export default PostForm;
