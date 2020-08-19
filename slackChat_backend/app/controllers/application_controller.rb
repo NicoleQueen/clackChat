886d1fff208b2db5d11fb4842a560f761d90c19d
NicoleQiu99 <nicolepeng99@gmail.com>
2020-08-18 21:22:30 -0700

    change some route

--- slackChat_backend/app/controllers/application_controller.rb
+++ slackChat_backend/app/controllers/application_controller.rb
@@ -1,41 +1,4 @@
 class ApplicationController < ActionController::API
-<<<<<<< HEAD
-  def encode_token(payload)
-    JWT.encode(payload, "its art")
-end 
-
-def auth_header
-# { Authorization: 'Bearer <token>' }
-request.headers['Authorization']
-end
-
-def decoded_token
-if auth_header
-  token = auth_header.split(' ')[1]
-  # header: { 'Authorization': 'Bearer <token>' }
-  begin
-    JWT.decode(token,'its art', true, algorithm: 'HS256')
-  rescue JWT::DecodeError
-    nil
-  end
-end
-end
-
-def current_user
-if decoded_token
-  user_id = decoded_token[0]['user_id']
-  @user = User.find_by(id: user_id)
-end
-end
-
-def logged_in?
-!!current_user
-end
-
-def authorized
-render json: { message: 'Please log in' }, status: :unauthorized unless logged_in?
-end
-=======
     def encode_token(payload)
         JWT.encode(payload, "its art")
     end 
@@ -71,5 +34,4 @@ end
   def authorized
     render json: { message: 'Please log in' }, status: :unauthorized unless logged_in?
   end
->>>>>>> 1333aa741e29bf957154813138b4b536c524b6c4
 end
--- slackChat_backend/db/migrate/20200818154603_change_pw_name.rb
+++ slackChat_backend/db/migrate/20200818154603_change_pw_name.rb
@@ -2,8 +2,4 @@ class ChangePwName < ActiveRecord::Migration[6.0]
   def change
     rename_column :users, :password, :password_digest
   end
-<<<<<<< HEAD
 end
-=======
-end
->>>>>>> 1333aa741e29bf957154813138b4b536c524b6c4
